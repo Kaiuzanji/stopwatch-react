96971e446b6850b4dcd321e829c4d29edc1b8f04
Paulo Andre <paulo.soares.ce@outlook.com>
2022-04-17 19:56:20 -0300

    finish project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0,
+      startButton: 'start'
+    }
+    this.timer = null
+    this.start = this.start.bind(this)
+    this.clear = this.clear.bind(this)
+  }
+
+  start(){
+    let state = this.state
+    if(this.timer){
+      clearInterval(this.timer)
+      this.timer = null
+      state.startButton = 'Start'
+    }else{
+      this.timer = setInterval(() => {
+        let state = this.state
+        state.count += 0.1
+        this.setState(state)
+      },100)
+      state.startButton = 'Stop'
+    }
+    this.setState(state)
+  }
+
+  clear(){
+    if(this.timer){
+      clearInterval(this.timer)
+      this.timer = null
+    }
+    let state = this.state
+    state.count = 0
+    state.startButton = 'Start'
+    this.setState(state)
+  }
+
+  render(){
+    return (
+      <div>
+        <h3>{this.state.count.toFixed(1)}</h3>
+        <div>
+          <button onClick={this.start}>{this.state.startButton}</button>
+          <button onClick={this.clear}>Limpar</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
